2f08429ca631f3ebec2e65c63b573d82e41a53c6
Matt Mondok <mattm@edentech.net>
2015-03-03 13:53:45 -0500

    License and headers

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Hadoop Mapper for Excel XLSX Input #This project is a Mapper for XLSX files.## Usage ##`hadoop jar [jar] [main_path] /path/to/file.xlsx /outputdir`
+# Hadoop Mapper for Excel XLSX Input #This project is a Mapper for XLSX files.  This is based on the work found in sreejithpillai's original ExcelRecordReaderMapReduce (https://github.com/sreejithpillai/ExcelRecordReaderMapReduce). ## Usage ##`hadoop jar [jar] [main_path] /path/to/file.xlsx /outputdir`
--- src/main/java/com/edentech/excelmapper/mapreduce/ExcelDriver.java
+++ src/main/java/com/edentech/excelmapper/mapreduce/ExcelDriver.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2014 Sreejith Pillai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * 2015 modifications for XLSX support and better List<T> passing by Matt Mondok
+*/
+
 package com.edentech.excelmapper.mapreduce;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
@@ -7,24 +27,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExcelDriver {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(ExcelDriver.class);
 
-	public static void main(String[] args) throws Exception {		
+	public static void main(String[] args) throws Exception {
 		logger.info("Driver started");
-		
+
 		Job job = new Job();
 		job.setJarByClass(ExcelDriver.class);
 		job.setJobName("Excel Record Reader");
-		
+
 		job.setMapperClass(ExcelMapper.class);
 		job.setNumReduceTasks(0);
-		
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
 		job.setInputFormatClass(ExcelInputFormat.class);
-		
+
 		job.waitForCompletion(true);
 	}
 
